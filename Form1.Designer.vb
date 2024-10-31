23acfa4c92ab11fb83ed9e2b55f2a4ad517dbb31
ayazakiamahjoub <ayazakia.mahjoub@isgb.ucar.tn>
2024-10-31 12:52:10 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(49, 34)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(569, 60)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaption
+        Button1.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(49, 136)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 67)
+        Button1.TabIndex = 1
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ActiveCaption
+        Button2.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(138, 136)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 67)
+        Button2.TabIndex = 2
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ActiveCaption
+        Button3.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(228, 136)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 67)
+        Button3.TabIndex = 3
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = SystemColors.ActiveCaption
+        Button4.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(49, 201)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 67)
+        Button4.TabIndex = 4
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = SystemColors.ActiveCaption
+        Button5.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(138, 201)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 67)
+        Button5.TabIndex = 5
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = SystemColors.ActiveCaption
+        Button6.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button6.Location = New Point(228, 201)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 67)
+        Button6.TabIndex = 6
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = SystemColors.ActiveCaption
+        Button7.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button7.Location = New Point(49, 267)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(94, 67)
+        Button7.TabIndex = 7
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = SystemColors.ActiveCaption
+        Button8.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button8.Location = New Point(138, 267)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(94, 67)
+        Button8.TabIndex = 8
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = SystemColors.ActiveCaption
+        Button9.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button9.Location = New Point(228, 267)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(94, 67)
+        Button9.TabIndex = 9
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = SystemColors.ActiveCaption
+        Button10.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button10.Location = New Point(49, 334)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(94, 67)
+        Button10.TabIndex = 10
+        Button10.Text = ","
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = SystemColors.ActiveCaption
+        Button11.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button11.Location = New Point(138, 334)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(94, 67)
+        Button11.TabIndex = 11
+        Button11.Text = "0"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = SystemColors.ActiveCaption
+        Button12.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button12.Location = New Point(228, 334)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(94, 67)
+        Button12.TabIndex = 12
+        Button12.Text = "AC"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = SystemColors.ActiveCaption
+        Button13.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button13.Location = New Point(500, 267)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(94, 67)
+        Button13.TabIndex = 13
+        Button13.Text = "/"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = SystemColors.ActiveCaption
+        Button14.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button14.Location = New Point(415, 201)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(94, 67)
+        Button14.TabIndex = 14
+        Button14.Text = "+"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = SystemColors.ActiveCaption
+        Button15.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button15.Location = New Point(500, 201)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(94, 67)
+        Button15.TabIndex = 15
+        Button15.Text = "-"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = SystemColors.ActiveCaption
+        Button16.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button16.Location = New Point(415, 267)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(94, 67)
+        Button16.TabIndex = 16
+        Button16.Text = "*"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = SystemColors.ActiveCaption
+        Button17.Font = New Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button17.Location = New Point(415, 136)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(179, 67)
+        Button17.TabIndex = 17
+        Button17.Text = "="
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = Color.Brown
+        Button18.Font = New Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button18.Location = New Point(415, 334)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(179, 70)
+        Button18.TabIndex = 18
+        Button18.Text = "Close"
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(641, 437)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Dim A As Double
+    Dim B As Double
+    Dim op As String
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click, Button11.Click, Button10.Click
+        TextBox1.Text &= DirectCast(sender, Button).Text
+    End Sub
+    Private Sub Operation_Click(sender As Object, e As EventArgs) Handles Button13.Click, Button14.Click, Button15.Click, Button16.Click
+        A = Double.Parse(TextBox1.Text)
+        op = DirectCast(sender, Button).Text
+        TextBox1.Clear()
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        B = Double.Parse(TextBox1.Text)
+        Dim result As Double
+
+        Select Case op
+            Case "+"
+                result = A + B
+            Case "-"
+                result = A - B
+            Case "*"
+                result = A * B
+            Case "/"
+                If B <> 0 Then
+                    result = A / B
+                Else
+                    MessageBox.Show("Division par zéro non autorisée.")
+                    Exit Sub
+                End If
+        End Select
+
+        TextBox1.Text = result.ToString()
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        TextBox1.Clear()
+        A = 0
+        B = 0
+        op = ""
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Me.Close()
+    End Sub
+End Class
